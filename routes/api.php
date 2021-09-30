ffdd98773f87432d53213ceb3db26127577164e5
Kafoma <kafoma@gmail.com>
2021-09-30 17:43:57 -0500

    Se crea un controlador especial para peticiones API

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\EmpleadosController;
+use App\Http\Controllers\Api\EmpleadosController;
 
 /*
 |--------------------------------------------------------------------------
